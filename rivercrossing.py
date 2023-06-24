c9c27c023ee5a2491c1b659c9f1213c1872d833e
calcuis <113646141+calcuis@users.noreply.github.com>
2023-06-24 12:09:09 +0800

    Update rivercrossing.py

--- rivercrossing.py
+++ rivercrossing.py
@@ -1,6 +1,4 @@
 # River Crossing Game
-# In this game, you have a farmer, a wolf, a goat, and a cabbage on the left bank of a river. The objective is to move all the items to the right bank without leaving the wolf alone with the goat or the goat alone with the cabbage. The player can choose to move one item at a time or move the boat (which requires the farmer to be in the boat). The game will display the state of the banks and the boat after each action, and it will check for win or lose conditions after each move.
-# Initialize the game
 left_bank = ['farmer', 'wolf', 'goat', 'cabbage']
 right_bank = []
 boat = []
